757d3a2a0da219feb40cf42feb13933db3d6700f
Clare <clarepan0@gmail.com>
2023-07-25 02:11:43 -0700

    set up page and navbar

---
+++ src/components/navBar.js
@@ -0,0 +1,35 @@
+import React from "react"
+import "./styles/navBar.css"
+
+function NavBar() {
+    return (
+        <div className="nav-container">
+            <div className="nav-bar">
+                <ul className="nav-list">
+                    <NavItem name={"Home"}/>
+                    <NavDivider/>
+                    <NavItem name={"About"}/>
+                    <NavDivider/>
+                    <NavItem name={"Projects"}/>
+                    <NavDivider/>
+                    <NavItem name={"Contact"}/>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+function NavDivider() {
+    return (
+        <li className="nav-divider">|</li>
+    );
+}
+
+function NavItem({name}) {
+    return (
+        <li className="nav-item">{name}</li>
+    );
+}
+
+
+export default NavBar
