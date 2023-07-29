bed422ecaa16e779d0e90e668f9f3c50cee373ba
Clare <clarepan0@gmail.com>
2023-07-28 23:42:10 -0700

    delete App.test.js and setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 // import {createContext, useState} from "react"
 import {useColorScheme} from "react-native"
-import {ThemeProvider} from 'styled-components';
-import {themes} from "./styles";
 
 import Header from "./components/header"
 import Homepage from "./pages/homepage";
@@ -9,27 +7,23 @@ import Homepage from "./pages/homepage";
 import "./App.css"
 
 // const ModeContext = createContext();
-
-
 function App() {
 
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider theme={colorScheme === 'dark' ? themes["dark"] : themes["light"]}>
-      <div className="App">
-        <div className="page-content">
+    <div className="App">
+      <div className="page-content">
 
-          <Header/>
-          <Homepage/>
-          <p>{colorScheme}</p>
-          <p>projects</p>
-          <p>contact</p>
-          <p>footer</p>
-          
-        </div>
+        <Header/>
+        <Homepage/>
+        <p>{colorScheme}</p>
+        <p>projects</p>
+        <p>contact</p>
+        <p>footer</p>
+        
       </div>
-     </ThemeProvider>
+    </div>
   );
 }
 
