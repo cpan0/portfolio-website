7b66eff3da3dd7f4512eabca3d532f3c4fdb80aa
Clare <clarepan0@gmail.com>
2023-07-29 23:23:14 -0700

    add light switch toggle

--- src/themeToggle.js
+++ src/themeToggle.js
@@ -1,6 +1,11 @@
 import { createContext, useContext, useState } from "react";
 import { useColorScheme } from "react-native";
 
+import lightOnImg from "./assets/images/lightswitch-on.png"
+import lightOffImg from "./assets/images/lightswitch-off.png"
+
+import "./themeToggle.css"
+
 const ThemeContext = createContext();
 
 function ThemeWrapper({ children }) {
@@ -31,7 +36,8 @@ function ToggleTheme() {
   
     return (
         <div className="toggle-theme"> 
-            <button onClick={toggle}>Go {altColorScheme}</button> 
+          <img className="toggleImg" src={colorScheme === "light" ? lightOnImg : lightOffImg} alt="light-switch-img" onClick={toggle} />
+          {/* <button onClick={toggle}>Go {altColorScheme}</button>  */}
         </div>
     );
 };
