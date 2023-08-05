3edcee4803898b434b4f10fb542e7fe749bc0fc4
Clare <clarepan0@gmail.com>
2023-08-04 21:27:44 -0700

    add ThursdayOk credits for artwork

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,12 +3,8 @@ import "./styles/footer.css"
 function Footer() {
     return (
         <div className="footer-container">
-            <div>
-            <p className="footer"> Made with &lt;3 by Clare :3</p>
-            </div>
-            <div>
-            <p className="footer"> Made with &lt;3 by Clare :3</p>
-            </div>
+            <p className="footer"> Made with &lt;3 by Clare :3 <br/> 
+            Special thanks to <a href="https://thursdayok.wixsite.com/portfolioreel">ThursdayOk</a> for the artworks!</p>
         </div>
     )
 };
